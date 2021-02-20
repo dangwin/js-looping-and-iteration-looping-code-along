083151159ba2f2ef526801d37707e789cccad496
dangwin <danghwin@gmail.com>
2021-02-19 23:13:33 -0600

    commit tree 60a81125c99f658d32ef437c2c3a6a1bc27eef7d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let array = []
+
+function writeCards(people, event) {
+    for(let i = 0; i < people.length; i++) {
+        array.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+}
+
+
+
+
+
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--)
+    }
+
+}
